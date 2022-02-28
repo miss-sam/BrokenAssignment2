5db0b52d53c935c9ce2ea38a9f30d295be45056c
TheAmyrlin95 <93338458+TheAmyrlin95@users.noreply.github.com>
2022-02-28 14:59:06 -0700

    Add files via upload

---
+++ BrokenBash.sh
@@ -0,0 +1,11 @@
+!#/bin/bash
+
+echo Hey There! Who the heck are you?
+
+read namevar
+
+echo Ohhhh, okay... Hey namevar how's it going... watch this!
+
+mv BrokenBash.sh TaDa.sh
+
+echo Done!
---
+++ BrokenPython.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+
+print("Hello!")
+
+name = str(input("Who are you?")
+
+print("Oh, hello "+ name +"!")
+
+question = str(input("Wanna answer a question?"))
+
+if question == 'no'
+    print("FINE!")
+elif question == 'yes':
+    print("Too Bad!")
+else:
+    print("Huh?")
